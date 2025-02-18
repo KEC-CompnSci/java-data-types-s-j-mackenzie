91ea9cbc239a5e696c786ca974250aec89a90b09
s-j-mackenzie <smackenzie689@gmail.com>
2025-02-18 17:26:48 +0000

    lab one

--- src/main/java/Assignment.java
+++ src/main/java/Assignment.java
@@ -20,11 +20,11 @@ public class Assignment {
     public Assignment() {
         // Students will initialize their variables here
         // Example (students should change these values):
-        myString = null;
-        myInt = 0;
-        myDouble = 0.0;
-        myBoolean = false;
-        myStringArray = null;
-        myIntArray = null;
+        myString = "hello world";
+        myInt = 654;
+        myDouble = 0.988;
+        myBoolean = true;
+        myStringArray = new String[]{"one", "two", "three"};
+        myIntArray = new int[]{34, 78, 9};
     }
 }
